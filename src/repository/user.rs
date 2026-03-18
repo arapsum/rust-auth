33ddaa8405ef7622fb2e9463d35d6ced1ab8f3ab
Kibet Bitok <kibetarapsum@gmail.com>
2026-03-18 11:18:42 +0300

    user sign-up service layer methods

--- src/repository/user.rs
+++ src/repository/user.rs
@@ -1,5 +1,5 @@
 use argon2::{
-    Argon2,
+    Argon2, PasswordHash, PasswordVerifier,
     password_hash::{PasswordHasher, SaltString, rand_core::OsRng},
 };
 use chrono::{DateTime, FixedOffset};
@@ -7,7 +7,7 @@ use serde::{Deserialize, Serialize};
 use sqlx::{Encode, Executor, Postgres, prelude::FromRow};
 use uuid::Uuid;
 
-use crate::validator::auth::RegisterUser;
+use crate::validator::auth::{LoginUser, RegisterUser};
 
 use super::{ModelError, ModelResult};
 
@@ -29,9 +29,9 @@ impl UserModel {
     where
         for<'a> &'a C: Executor<'e, Database = Postgres>,
     {
-        let password_hash = Self::hash_password(params.password().trim())?;
+        let password_hash: String = Self::hash_password(params.password().trim())?;
 
-        let new_user = sqlx::query_as::<_, Self>(
+        let new_user: Self = sqlx::query_as::<_, Self>(
             r"
             INSERT INTO users (email, password_hash, username)
             VALUES ($1, $2, $3)
@@ -47,13 +47,67 @@ impl UserModel {
         Ok(new_user)
     }
 
+    pub async fn sign_in_user<'e, C>(db: &C, params: &LoginUser<'_>) -> ModelResult<Self>
+    where
+        for<'a> &'a C: Executor<'e, Database = Postgres>,
+    {
+        let user: Self = Self::find_user_by_email(db, params.email())
+            .await?
+            .ok_or_else(|| ModelError::InvalidCredentials)?;
+
+        user.verify_password(params.password().trim())?;
+
+        Ok(user)
+    }
+
+    pub async fn find_user_by_id<'e, C>(db: &C, id: Uuid) -> ModelResult<Self>
+    where
+        for<'a> &'a C: Executor<'e, Database = Postgres>,
+    {
+        let user: Self = sqlx::query_as::<_, Self>(
+            r"
+            SELECT * FROM users WHERE id = $1
+        ",
+        )
+        .bind(id)
+        .fetch_optional(db)
+        .await?
+        .ok_or_else(|| ModelError::EntityNotFound)?;
+
+        Ok(user)
+    }
+
+    pub async fn find_user_by_email<'e, C>(db: &C, email: &str) -> ModelResult<Option<Self>>
+    where
+        for<'a> &'a C: Executor<'e, Database = Postgres>,
+    {
+        let user: Option<Self> = sqlx::query_as::<_, Self>(
+            r"
+            SELECT * FROM users WHERE email = $1
+        ",
+        )
+        .bind(email)
+        .fetch_optional(db)
+        .await?;
+
+        Ok(user)
+    }
+
     fn hash_password(password: &str) -> ModelResult<String> {
-        let argon = Argon2::default();
-        let salt = SaltString::generate(&mut OsRng);
+        let argon: Argon2<'_> = Argon2::default();
+        let salt: SaltString = SaltString::generate(&mut OsRng);
 
         Ok(argon
             .hash_password(password.as_bytes(), &salt)
             .map_err(ModelError::PasswordHash)?
             .to_string())
+    }
+
+    fn verify_password(&self, plain_password: &str) -> ModelResult<()> {
+        let parded_hash = PasswordHash::new(&self.password_hash)?;
+
+        Argon2::default().verify_password(plain_password.as_bytes(), &parded_hash)?;
+
+        Ok(())
     }
 }
